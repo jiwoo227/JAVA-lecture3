a9ce2a1a476bcbea6aa5bbfd1bdb43eebdf101e0
최지우 <w2040@e-mirim.hs.kr>
2022-09-13 20:57:39 +0900

    조건문

---
+++ 조건문/src/IfEx.java
@@ -0,0 +1,13 @@
+public class IfEx {
+    public static void main(String[] args){
+        int x = 50;
+        int y = 60;
+
+        if(x<y){
+            System.out.println("x는 y보다 작다.");
+        }else{
+            System.out.print("크다!" +
+                    "");
+        }
+    }
+}
---
+++ 조건문/src/SwitchEx01.java
@@ -0,0 +1,19 @@
+public class SwitchEx01 {
+    public static void main(String[] args){
+        //switch, case, default, break
+
+        int value = 1;
+
+        switch (value){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("그 외 다른 숫자");
+                break;
+        }
+    }
+}
