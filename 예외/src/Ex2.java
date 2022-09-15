7230e65f4dc62afe5a289d1c590f217773250fd7
최지우 <w2040@e-mirim.hs.kr>
2022-09-16 00:21:09 +0900

    throws

---
+++ 예외/src/Ex2.java
@@ -0,0 +1,16 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 0;
+        try {
+            int k = divide(i, j);
+        }catch (ArithmeticException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static int divide(int i, int j) throws ArithmeticException, ClassCastException{
+        int k = i/j;
+        return k;
+    }
+}
